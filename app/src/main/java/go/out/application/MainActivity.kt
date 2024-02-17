16cdadfc41867f5a03cfa05790328edc541c6a1f
DanieleFavarin <daniele.favarin86@gmail.com>
2024-02-17 10:25:21 +0100

    prima versione del layout schermata principale utente

--- app/src/main/java/go/out/application/MainActivity.kt
+++ app/src/main/java/go/out/application/MainActivity.kt
@@ -28,13 +28,14 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -80,7 +81,7 @@ class MainActivity : AppCompatActivity() {
 
         builder.setPositiveButton("Ricerca") { dialog, _ ->
             val email = inputEmail.text.toString()
-           FirebaseDBHelper.searchFriendByEmail(email)
+         //  FirebaseDBHelper.searchFriendByEmail(email)
             dialog.dismiss()
         }
 
